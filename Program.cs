10e5cef3d9bd3dd4ceaf8b8822946168c7e6351c
Pietra <pietracosta.dev@gmail.com>
2025-05-08 23:20:41 -0300

    coment

--- Program.cs
+++ Program.cs
@@ -1,29 +1,55 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from Railway!");
-
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://*:{port}");
-// Configure the HTTP request pipeline.
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 
-app.UseSwagger();
- app.UseSwaggerUI();
-
-
-//app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment() || true) 
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
+
 app.MapControllers();
+app.MapGet("/", () => "Hello from Railway!");
 
 app.Run();
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello from Railway!");
+
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//app.Urls.Add($"http://*:{port}");
+//// Configure the HTTP request pipeline.
+
+
+//app.UseSwagger();
+// app.UseSwaggerUI();
+
+
+////app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
